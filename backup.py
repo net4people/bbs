a13dbb0371ab9c5cc14463b489bb4405b88eed14
Marek Küthe <m.k@mk16.de>
2026-01-23 20:51:37 +0000

    Switch to mistune 3
    Add mistune 3 compatibiliy
    
    Closes https://github.com/net4people/bbs/issues/565, https://github.com/net4people/bbs/pull/566
    
    Signed-off-by: Marek Küthe <m.k@mk16.de>
    
    Remove compatibility with mistune 0 and mistune 2
    
    Signed-off-by: Marek Küthe <m.k@mk16.de>

--- backup.py
+++ backup.py
@@ -134,28 +134,19 @@ def get_to_tempfile(sess, url, mediatype, params={}):
         tmp.write(chunk)
     return tmp
 
-# Fallback to mistune 1.0 renderer if mistune 2.0 is not installed
-try:
-    mistuneRenderer = mistune.HTMLRenderer
-except AttributeError:
-    mistuneRenderer = mistune.Renderer
 # Custom mistune.Renderer that stores a list of all links encountered.
-class LinkExtractionRenderer(mistuneRenderer):
+class LinkExtractionRenderer(mistune.HTMLRenderer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.links = []
 
-    def autolink(self, link, is_email=False):
-        self.links.append(link)
-        return super().autolink(link, is_email)
+    def image(self, text, url, title=None):
+        self.links.append(url)
+        return super().image(text, url, title)
 
-    def image(self, src, title, alt_text):
-        self.links.append(src)
-        return super().image(src, title, alt_text)
-
-    def link(self, link, title, content=None):
-        self.links.append(link)
-        return super().link(link, title, content)
+    def link(self, text, url, title=None):
+        self.links.append(url)
+        return super().link(text, url, title)
 
 def markdown_extract_links(markdown):
     renderer = LinkExtractionRenderer()
